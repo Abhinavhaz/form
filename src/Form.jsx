cac718d8cb0337fcd30c3aa3adc6807d7528b3e6
Abhinav hazarika <abhinavhazarika27@gmail.com>
2024-05-09 21:04:53 +0530

    form

---
+++ src/Form.jsx
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+function Form() {
+  const [firstName, setFirstName] = useState("");
+  const [LastName, setLastName] = useState("");
+  const [fullName, setFull] = useState("");
+  const [yes, setYes] = useState(false);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const fullName = `${firstName} ${LastName}`;
+    setFull(fullName);
+    setYes(true);
+    setFirstName("");
+    setLastName("");
+  };
+
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <h1>Full Name Display </h1>
+        <label>
+          First Name:
+          <input
+            required
+            type="text"
+            value={firstName}
+            onChange={(e) => setFirstName(e.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Last Name:
+          <input
+            required
+            value={LastName}
+            onChange={(e) => setLastName(e.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Submit </button>
+        {yes && <p>Full Name:{fullName} </p>}
+      </form>
+    </div>
+  );
+}
+export default Form;
